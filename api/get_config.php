46081e4db53bcddb6b19e661e20aa090bf35067f
snepple <25994154+snepple@users.noreply.github.com>
2026-04-14 14:28:26 +0000

    Merge origin/main to fix package.json conflict

--- api/get_config.php
+++ api/get_config.php
@@ -4,7 +4,9 @@ $file = '../config.json';
 if (file_exists($file)) {
     // Read the file, decode it, remove the password, re-encode it
     $data = json_decode(file_get_contents($file), true);
-    unset($data['admin_password']); 
+    unset($data['admin_password']);
+    unset($data['api_integrations']['gemini_api_key']);
+    unset($data['dashboard_token']);
     echo json_encode($data);
 } else {
     echo json_encode(['error' => 'Config file missing']);
